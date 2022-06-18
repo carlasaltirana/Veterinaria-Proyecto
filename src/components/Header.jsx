61eff466db622ed4e525c41203034797edc91df2
carlasaltirana <saltirana.carla@gmail.com>
2022-06-18 18:04:41 -0300

    Primer commit del proyecto

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header({}){
+
+    return(
+        <>
+           <h1 className= "font-serif text-5xl text-center md:w-2/3 mx-auto"> Seguimiento Pacientes {''}
+           <span className= "font-mono text-5xl text-center text-yellow-100">Veterinaria</span>
+           </h1> 
+        </>
+    )
+}
+
+export default Header;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{jsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    colors: {
+      // Configure your color palette here
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
